afb75cbcd66ffd7e191cd6b83ffcb0a86bc89608
andybeet <22455149+andybeet@users.noreply.github.com>
2019-10-25 12:11:23 -0400

    @export functions

--- R/createStringSpecies.R
+++ R/createStringSpecies.R
@@ -1,23 +1,23 @@
-##' Utility function (Internal, not exported) to convert argument to strings
-##'
-##' Deal explicity with NESPP3 and NESPP4 codes.
-##' inputs to get_ functions are required as strings when passed as a sql statement
-##' This function converts numeric inputs to character strings
-##'
-##' @param itemName Character string. Variable name as it exists in the data base
-##' @param chosenItem User input value of variable listed in itemName
-##' @param convertTocharacter Boolean. Should we convert the \code{chosenItem} to a character string (This depends on how the variable is declared in the database)
-##' @param numchars Numeric scalar. Number of characters to format the numeric \code{chosenItem} as.
-##'
-##' @return A charachter string
-##'
-##' @examples
-##' \dontrun{
-##' createStringSpecies(itemName="area",area=503,convertToCharacter=TRUE,numChars=3)
-##' createStringSpecies(itemName="species_itis",species,convertToCharacter=TRUE,numChars=6)
-##'
-##' }
-##'
+#' Utility function (Internal, not exported) to convert argument to strings
+#'
+#' Deal explicity with NESPP3 and NESPP4 codes.
+#' inputs to get_ functions are required as strings when passed as a sql statement
+#' This function converts numeric inputs to character strings
+#'
+#' @param itemName Character string. Variable name as it exists in the data base
+#' @param chosenItem User input value of variable listed in itemName
+#' @param convertTocharacter Boolean. Should we convert the \code{chosenItem} to a character string (This depends on how the variable is declared in the database)
+#' @param numchars Numeric scalar. Number of characters to format the numeric \code{chosenItem} as.
+#'
+#' @return A charachter string
+#'
+#' @examples
+#' \dontrun{
+#' createStringSpecies(itemName="area",area=503,convertToCharacter=TRUE,numChars=3)
+#' createStringSpecies(itemName="species_itis",species,convertToCharacter=TRUE,numChars=6)
+#'
+#' }
+#'@export
 
 
 
