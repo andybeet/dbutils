5ae778515fc18a45be68ee5e580fb9953a3fa2d3
andybeet <22455149+andybeet@users.noreply.github.com>
2021-09-13 12:41:40 -0400

    explicitly reference base package

--- R/capture_function_call.R
+++ R/capture_function_call.R
@@ -2,18 +2,20 @@
 #'
 #' A function call and its arguments, including default arguments, are captured and returned as class call
 #'
+#'@param ... F
+#'
 #' @return the function call
 #'
 #'
 #'@export
 
 capture_function_call <- function(...) {
-  call <- evalq(match.call(expand.dots = FALSE), parent.frame(1))
-  formals <- evalq(formals(), parent.frame(1))
+  call <- base::evalq(base::match.call(expand.dots = FALSE), parent.frame(1))
+  formals <- base::evalq(formals(), parent.frame(1))
   
-  for(i in setdiff(names(formals), names(call))){
+  for(i in base::setdiff(names(formals), names(call))){
     call[i] <- list( formals[[i]] )
   }
   
-  return(match.call(sys.function(sys.parent()), call))
+  return(base::match.call(base::sys.function(base::sys.parent()), call))
 }
