c708965cf4f70dd3a71f596cbb4e3953a5039e68
andybeet <22455149+andybeet@users.noreply.github.com>
2021-12-15 13:41:09 -0500

    added package R file

---
+++ R/dbutils.r
@@ -0,0 +1,13 @@
+#' dbutils: Utility functions to aid in database interactions
+#'
+#' 
+#'
+#'\itemize{
+#'\item Connect to databases
+#'\item Capture function calls
+#'\item SQL statement formatting functions
+#'}
+#'
+#' @docType package
+#' @name dbutils
+NULL
